af9e0efc23930c5e7629ac07b9589306cad4297a
Charles Vestal <charlesv@gmail.com>
2020-02-13 10:32:59 +0100

    Adding in codepen css

--- forbes.sh
+++ forbes.sh
@@ -3,6 +3,7 @@ a=1
 
 echo "<!DOCTYPE html>
 <head>
+<link rel=stylesheet href=https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css>
 <link rel="stylesheet" href="./style.css">
 </head>
 <body>
